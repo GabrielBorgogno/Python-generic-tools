91355ed73acf8a465f9c6e135f72ad65c23df636
GabrielBorgogno <92234529+GabrielBorgogno@users.noreply.github.com>
2022-08-16 00:23:00 -0300

    111

---
+++ 111. Herança Simples - Python Orientado a Objetos/Classes.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.nome= nome
+        self.idade= idade
+        self.nomeclasee=self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasee} Falando...')
+
+class Cliente(Pessoa):
+     def comprar(self):
+        print(f'{self.nomeclasee} comprando...')
+
+class Aluno (Pessoa):
+    def estudar(self):
+        print(f'{self.nomeclasee} estudando...')
---
+++ 111. Herança Simples - Python Orientado a Objetos/Main.py
@@ -0,0 +1,9 @@
+from Classes import *
+
+c1 = Cliente('gabriel',24)
+print(c1.nome)
+c1.falar()
+c1.comprar()
+a1=Aluno('Joj',25)
+a1.estudar()
+a1.falar()
